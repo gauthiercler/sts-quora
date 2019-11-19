4b28b2f3449e45516a78ced4d659e967a7f26629
Gauthier Cler <gauthier.cler@epitech.eu>
2019-11-19 10:38:29 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Sentence to sentence semantic similarity',
+    author='Gauthier Cler',
+    license='MIT',
+)
